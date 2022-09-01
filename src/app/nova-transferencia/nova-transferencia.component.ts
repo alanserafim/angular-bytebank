2c0d6d3e78bb40775301a15ce753d3aa43308354
alanserafim <alan.serafim@fatec.sp.gov.br>
2022-09-01 00:03:33 -0300

    refaoração

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent implements OnInit {
+                                          //generic
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
@@ -17,6 +19,8 @@ export class NovaTransferenciaComponent implements OnInit {
 
   transferir() {
     console.log("Solicitada nova transferência");
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
     console.log("Valor: ", this.valor);
     console.log("Destino: ", this.destino);
 
